54caf86d259a80ad49f06de563f683553dbf4e7c
Hallom <challom@pharmalogistics.com>
2020-04-23 20:13:21 -0500

    added a check for blank or null input.

--- calcScript.js
+++ calcScript.js
@@ -3,8 +3,14 @@
 function enterButton(button){
     
     var numWin = document.getElementById("numWindow");
-    numWin.value = parseInt(numWin.value) + parseInt(button.value);
-    console.log(numWin.value);
+    if(numWin.value == ""){
+        numWin.value = numWin.value + "0";
+    }
+    else{
+        numWin.value = parseInt(numWin.value) + parseInt(button.value);
+        console.log(numWin.value);
+    }
+    
 
 
 }
