61feef2cd4e9c204c9eba4edcf463837202b2b59
Hallom <challom@pharmalogistics.com>
2020-04-23 19:46:38 -0500

    first step completed, can put numbers in entry.

---
+++ calcScript.js
@@ -0,0 +1,12 @@
+//document.getElementById("numOne").addEventListener("click", enterButton);
+
+
+function enterButton(){
+    
+    var numWin = document.getElementById("numWindow");
+
+    numWin.value = parseInt(numWin.value) + 3;
+    console.log(numWin.value);
+
+    // Figured out adding values and changing the text. 
+}
