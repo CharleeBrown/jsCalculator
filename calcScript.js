b7672cfb8595a9786b6e1e2b968996f0b8c58fa5
Hallom <challom@pharmalogistics.com>
2020-04-27 19:20:27 -0500

    Added all buttons. Getting Nan on negatives.

--- calcScript.js
+++ calcScript.js
@@ -27,8 +27,39 @@ function pemdas(button){
 function getValue(){
      var getStrings = numWin.value.toString();
      var getarr = getStrings.split(" ");
-    console.log(Math.Add(parseInt(getarr[0])  + getarr[1] + parseInt(getarr[2]));
-     numWin.value = "";
+     if(getarr[1].value = "+"){
+         var results = parseInt(getarr[0]) + parseInt(getarr[2]);
+         console.log(results);
+         numWin.value = results;
+     }
+     else if (getarr[1].value ="-"){
+         if(parseInt(getarr[0]) > parseInt(getarr[2])){
+             results = parseInt(getarr[0]) - parseInt(getarr[2]);
+            }
+            else if(parseInt(getarr[2]) > parseInt(getarr[0]))
+            {
+                results = parseInt(getarr[2]) - parseInt(getarr[0]);
+            }
+            console.log(results);
+            /* if(results < 0){
+                  results = parseInt(results) * -1; 
+                 console.log("-" + results);*/
+             numWin.value = "-" + results;     
+    }
+     else if (getarr[1].value ="*"){
+        var results = parseInt(getarr[0]) * parseInt(getarr[2]);
+        console.log(results);
+        numWin.value = results;
+     }
+        else if (getarr[1].value ="/"){
+            var results = parseInt(getarr[0]) % parseInt(getarr[2]);
+            console.log(results);
+            numWin.value = results;
+        }
+  //  console.log(Math.Add(parseInt(getarr[0])  + getarr[1] + parseInt(getarr[2]));
+    
 }
+
+
 
 
