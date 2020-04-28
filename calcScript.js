be6c2e90d0e94b1af5aaafadaeb7dccb1615ca0f
Hallom <challom@pharmalogistics.com>
2020-04-27 20:12:20 -0500

    Brougt it back to when only addition worked.

--- calcScript.js
+++ calcScript.js
@@ -10,9 +10,8 @@ function enterButton(button){
     if(numWin.value == ""){
         numWin.value = parseInt(button.value);
     }
-     
     else{
-        numWin.value = numWin.value +" "+ button.value;
+        numWin.value = numWin.value + " " + button.value;
     }
 }
 
@@ -26,10 +25,32 @@ function pemdas(button){
 }
 
 function getValue(){
-    
      var getStrings = numWin.value.toString();
      var getarr = getStrings.split(" ");
-    console.log(getarr);
+     if(getarr[1].value = "+"){
+         var results = parseInt(getarr[0]) + parseInt(getarr[2]);
+         console.log(results);
+         numWin.value = results;
+     }
+     else if (getarr[1].value ="-"){
+        var results = parseInt(getarr[0]) - parseInt(getarr[2]);
+        if(results < 0){
+             results = parseInt(results) * -1; 
+            console.log("-" + results);
+        numWin.value = "-" + results;
+     }
+    }
+     else if (getarr[1].value ="*"){
+        var results = parseInt(getarr[0]) * parseInt(getarr[2]);
+        console.log(results);
+        numWin.value = results;
+     }
+        else if (getarr[1].value ="/"){
+            var results = parseInt(getarr[0]) % parseInt(getarr[2]);
+            console.log(results);
+            numWin.value = results;
+        }
+  //  console.log(Math.Add(parseInt(getarr[0])  + getarr[1] + parseInt(getarr[2]));
     
 }
 
