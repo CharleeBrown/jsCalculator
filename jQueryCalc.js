db047dffd4e3bdd29b8f0a3c457f7ed72a6c8507
Hallom <challom@pharmalogistics.com>
2020-05-04 19:38:09 -0500

    formatting

--- jQueryCalc.js
+++ jQueryCalc.js
@@ -1,27 +1,25 @@
-$(document).ready(function(){
-     var numWin = $('#numWindow')[0];
+$(document).ready(function () {
+    var numWin = $('#numWindow')[0];
 
 
-    $.fn.clearIt = function(){
+    $.fn.clearIt = function () {
         numWin.value = "";
-        };
-    $("#num").click(function(){
-        if(numWin.value == ""){
+    };
+    $("#num").click(function () {
+        if (numWin.value == "") {
             numWin.value = parseInt($(this).val());
             console.log($(this).val());
-            }
-        else{
-        numWin.value = numWin.value + ""+ $(this).val();
-             }
+        } else {
+            numWin.value = numWin.value + "" + $(this).val();
+        }
     });
 
-    $.fn.pemdas  = function(){
-           if(numWin.value ==""){
-                numWin.value = "";
-            }
-         else{
-        numWin.value = numWin.value + ""+ $(this).val();
-            }       
+    $.fn.pemdas = function () {
+        if (numWin.value == "") {
+            numWin.value = "";
+        } else {
+            numWin.value = numWin.value + "" + $(this).val();
+        }
     }
-    
+
 });
