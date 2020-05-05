89a5778035a9afeff09c4eea7b3581274d7a92da
Hallom <challom@pharmalogistics.com>
2020-05-04 19:28:28 -0500

    Fixing Formatting.

--- calcScript.js
+++ calcScript.js
@@ -2,64 +2,58 @@ var numWin = document.getElementById("numWindow");
 var tests = document.getElementById("testWindow");
 numWin.addEventListener("keydown", getkey);
 //body.addEventListener("keydown", getkey);
- document.addEventListener("keydown", getkey);
+document.addEventListener("keydown", getkey);
 
-function getkey(event){
+function getkey(event) {
     numWin.focus();
-   if(event.keyCode == 13){
-       getValue();
-   }
-   if(event.keyCode == 46){
-       clearIt();
-   }
-   console.log(event.keyCode);
+    if (event.keyCode == 13) {
+        getValue();
+    }
+    if (event.keyCode == 46) {
+        clearIt();
+    }
+    console.log(event.keyCode);
 }
-function clearIt(){
+
+function clearIt() {
     document.getElementById("numWindow").value = "";
 }
 
-function enterButton(button){
-    
-    var checks = numWin.split(" "); 
-    if(checks[0].value== null){
+function enterButton(button) {
+
+    var checks = numWin.split(" ");
+    if (checks[0].value == null) {
         numWin.value = parseInt(button.value);
-    }
-    else if(checks[1] == null){
-        numWin.value ="";
-        numWin.value =  parseInt(button.value);
-    }
-    else{
+    } else if (checks[1] == null) {
+        numWin.value = "";
+        numWin.value = parseInt(button.value);
+    } else {
         numWin.value = numWin.value + " " + button.value;
     }
 }
 
-function pemdas(button){
-    if(numWin.value == ""){
+function pemdas(button) {
+    if (numWin.value == "") {
         numWin.value = "";
-    }
-    else{
+    } else {
         numWin.value = numWin.value + " " + button.value;
     }
 }
 
 var errorShow = document.getElementById("errorShow");
-function getValue(){
-     try{
-     numWin.value = eval(numWin.value);
-     }
-     catch(error){
-         if(err = "SyntaxError: \"missing ) in parenthetical\""){
+
+function getValue() {
+    try {
+        numWin.value = eval(numWin.value);
+    } catch (error) {
+        if (err = "SyntaxError: \"missing ) in parenthetical\"") {
             errorShow.innerHTML = "Close Parenthesis";
             var doTime = setTimeout(clearError(), 300);
             clearTimeout(doTime);
-         }
-     }
+        }
+    }
 }
 
-function clearError(){
+function clearError() {
     errorShow.innerHTML = "";
 }
-
-
-
-
